a1d39be1d82cb2ce26a4ebb074a6cee0f2d457c3
Anatoliy Salamatin <counter18@yandex.ru>
2022-07-08 12:54:59 +0300

    Рефакторинг

--- src/app/shared/services/bd/user-bd.service.ts
+++ src/app/shared/services/bd/user-bd.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { GetSupabaseClientService } from './get-supabase-client.service';
 import { EBdTables } from '../../enum/bd-tables.enum';
-import { from, Observable, pluck } from 'rxjs';
+import { from, map, Observable, pluck } from 'rxjs';
 import { IProfile } from '../../../modules/profile/interfaces/profile.interface';
 
 @Injectable({
@@ -14,21 +14,36 @@ export class UserBdService {
   ) {
   }
 
-  get session() {
+  public get session() {
     return this.getSupabaseClientService.getSupabaseClient().auth.session();
   }
 
-  get user() {
+  public get user() {
     return this.getSupabaseClientService.getSupabaseClient().auth.user();
   }
 
-  get profile(): Observable<IProfile> {
+  public get profile(): Observable<IProfile> {
     return from(this.getSupabaseClientService.getSupabaseClient()
       .from(EBdTables.USERS)
       .select(`email,name,surName,patronymic,dateOfBirth,id`)
       .eq('id', this.user?.id)
       .single()).pipe(
+      map((res: any) => this.checkEmptyUser(res)),
       pluck('data')
     );
+  }
+
+  private checkEmptyUser(res: any): { data:IProfile } {
+    if (res?.error?.code === 'PGRST116') {
+      return {
+        data: {
+          name: '',
+          surName: '',
+          patronymic: '',
+          dateOfBirth: ''
+        }
+      }
+    }
+    return res;
   }
 }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  supabaseUrl: "https://smrmwcmchwdvmybtdilh.supabase.co",
+  supabaseKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNtcm13Y21jaHdkdm15YnRkaWxoIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NTcxOTI4MzgsImV4cCI6MTk3Mjc2ODgzOH0.drfZ9p85KUmO_SQN8MA64F-xjsu3Xt-mANjE0dhM1Z0"
 };
